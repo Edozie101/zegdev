89f1ba6a9fe883408f816ddcc7f7d0ea6a10a95d
edzye101 <edzye101@gmail.com>
2015-12-09 14:38:32 +0000

    added the next spot

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'heroku'
 gem 'sinatra'
 gem 'sprockets'
+gem 'pony'
--- app.rb
+++ app.rb
@@ -3,3 +3,33 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/mail' do
+  require 'pony'
+  person = params
+  puts params
+  name = person[name]
+  email = person[email]
+  message = person[message]
+
+
+
+  Pony.mail({
+    :to => 'eai267@g.harvard.edu',
+    :from => email,
+    :subject => "#{name}  has contacted you about ZegDev",
+    :body => "\n \n #{message} \n sent from #{name}",
+    :via => :smtp,
+    :via_options => {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+    })
+
+
+end
